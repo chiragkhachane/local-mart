da28648ce5966deae4d590f698ac9e46a43dc1da
Chirag Khachane <40366726+chiragkhachane@users.noreply.github.com>
2025-07-01 12:52:25 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -11,17 +11,17 @@ const config = {
   //   (process.env.MONGO_PORT || "27017") +
   //   "/NodeAPI",
 
-  stripe_connect_test_client_id: "ca_IpdSleJKRHel8sQB402NTFQbWmxwuuNy",
+  stripe_connect_test_client_id: "",
   stripe_test_secret_key:
-    "sk_test_51IDxvmGENzKU2dPwmkzTnR1v2P8JseEiAdDQbsxoPJ3AY0pRyrtMe5aT6ZOn3sD5NNGPMPXMlyEsVxhoMcc2UjCd00ytwJqUVR",
+    "",
   stripe_test_api_key:
-    "pk_test_51IDxvmGENzKU2dPwn1CIhOAKtRn8RY78Gg6LeUNlq6GOtbS6MWBLCCohf2WbgACsyqMrbY4xdj6n4SZ3McFVbjUF003NlTJ0ex",
+    "",
   AWS_ACCESS_KEY_ID: "",
   AWS_SECRET_ACCESS_KEY: "",
   AWS_REGION: "us-east-1",
-  EMAIL_FROM: "persistent2021@gmail.com",
-  EMIAL_PW: "beproject",
-  GEOCODER_API_KEY: "AIzaSyCgprdJj4mS8pDdtsctNoEJTQTAEvmeyqE",
+  EMAIL_FROM: "",
+  EMIAL_PW: "",
+  GEOCODER_API_KEY: "",
 };
 
 export default config;
